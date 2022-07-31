ffff0af906ad18fc66b950a9c4a2cb15f5e5fccc
Benard Andrew <andrewbenard7@gmail.com>
2022-07-31 11:21:58 +0300

    Anon Login changes

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -4,7 +4,7 @@ import 'package:facilities_booking_unionsuites/pages/home.dart';
 import 'package:facilities_booking_unionsuites/pages/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
